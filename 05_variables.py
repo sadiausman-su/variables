720bc876e6f5898833597cd50b9a159c46c64f8e
sadiausman-su <usmanbodla047@gmail.com>
2025-03-06 15:54:34 +0500

    Add files via upload

---
+++ 05_variables.py
@@ -0,0 +1,22 @@
+#variabels containing specific value
+x=5
+print(x)
+y="we are learnig python with self"
+print(y)
+x=x+5
+print(x)
+#types of variables
+print(type(x))
+print(type(y))
+#rules for variables
+#1. variable name should be a string of characters
+#2. variable name should not be a number
+#3. variable name should not be a keyword
+#4. variable name should not be a special character
+#5. variable name should not be a space
+#6. case sensitivity
+#7. variable name should be a single word
+
+fruit_basket="mangos,oranges"
+print(fruit_basket)
+print(type(fruit_basket))
